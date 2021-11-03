0faca2f400c840183a356b401612be2cd1800e9b
Andrey Arkhipov <arhipov@yandex.ru>
2021-11-03 10:34:10 +0300

    add/form-edit

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,15 @@
 
 const profile = document.querySelector(".profile");
 
-//Открытие и закрытие popup
-function popupOpenClose() {
-  const popupEditProfile = document.querySelector(".popup_name_edit-profile");
-  const popupAddPlace = document.querySelector(".popup_name_new-place");
-  const btnEditProfile = profile.querySelector(".profile__edit-button");
-  const btnAddPlace = profile.querySelector(".profile__add-button");
-  const btnClosePopup = document.querySelectorAll(".popup__close-button");
-  const opened = "popup_opened";
+//Открытие popup
+const popupEditProfile = document.querySelector(".popup_name_edit-profile");
+const popupAddPlace = document.querySelector(".popup_name_new-place");
+const btnEditProfile = profile.querySelector(".profile__edit-button");
+const btnAddPlace = profile.querySelector(".profile__add-button");
+const btnClosePopup = document.querySelectorAll(".popup__close-button");
+const opened = "popup_opened";
 
+function popupOpen() {
   function openPopup(el, str) {
     el.classList.add(str);
   }
@@ -18,8 +18,12 @@ function popupOpenClose() {
   btnEditProfile.addEventListener("click", () =>
     openPopup(popupEditProfile, opened)
   );
-  btnAddPlace.addEventListener("click", () => openPopup(popupAddPlace, opened));
 
+  btnAddPlace.addEventListener("click", () => openPopup(popupAddPlace, opened));
+}
+
+//Закрытие попап
+function popupClose() {
   for (let btn of btnClosePopup) {
     btn.addEventListener("click", function () {
       popupEditProfile.classList.remove(opened);
@@ -28,4 +32,28 @@ function popupOpenClose() {
   }
 }
 
-popupOpenClose();
+//Редактирование формы
+function formEdit() {
+  const formEdit = document.querySelector(".form_type_edit-profile");
+  const formNameItem = formEdit.querySelector(".form__item_type_name");
+  const formMissionItem = formEdit.querySelector(".form__item_type_mission");
+  const profileName = profile.querySelector(".profile__name");
+  const profileMission = profile.querySelector(".profile__mission");
+
+  formNameItem.value = profileName.textContent;
+  formMissionItem.value = profileMission.textContent;
+
+  function formSubmitHandler(event) {
+    event.preventDefault();
+    profileName.textContent = formNameItem.value;
+    profileMission.textContent = formMissionItem.value;
+    popupEditProfile.classList.remove(opened);
+    popupAddPlace.classList.remove(opened);
+  }
+
+  formEdit.addEventListener("submit", formSubmitHandler);
+}
+
+popupOpen();
+popupClose();
+formEdit();
