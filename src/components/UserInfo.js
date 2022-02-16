39cfa1a33e97bb2412054af0371c505432cb92df
Andrey Arkhipov <arhipov@yandex.ru>
2022-02-16 18:12:39 +0300

    Added userInfo class

---
+++ src/components/UserInfo.js
@@ -0,0 +1,22 @@
+export class UserInfo {
+  constructor({ profileName, profileMission, profileAvatar }) {
+    this._name = document.querySelector(profileName);
+    this._about = document.querySelector(profileMission);
+    this._avatar = document.querySelector(profileAvatar);
+  }
+
+  getUserInfo(data) {
+    if (!this._user) {
+      this._user = data;
+      return this._user;
+    } else {
+      return this._user;
+    }
+  }
+
+  setUserInfo() {
+    this._name.textContent = this._user.name;
+    this._about.textContent = this._user.about;
+    this._avatar.src = this._user.avatar;
+  }
+}
--- src/components/api.js
+++ src/components/api.js
@@ -102,7 +102,7 @@ export default class Api {
     if (res.ok) {
       return res.json();
     }
-    return Promise.reject(`Ошибка: ${res.status}`)
+    return Promise.reject(`Ошибка: ${res.status}`);
   }
 
   deleteCard(cardId) {
