3b90cbb28175e074fb1773d70a12869020807dde
Andrey Arkhipov <arhipov@yandex.ru>
2022-02-16 19:48:20 +0300

    Added PopupWithImage class

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,13 @@
+export default class PopupWithImage extends Popup {
+  _title = document.querySelector(".popup__title_for_image");
+  _image = document.querySelector(".popup__image");
+  constructor(popupSelector) {
+    super(popupSelector);
+  }
+
+  open(card) {
+    super.open();
+    this._title.textContent = card.title;
+    this._image.src = card.image;
+  }
+}
