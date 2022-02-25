638ed553acb65a2530749ecfb85fcfe05d61f704
Andrey Arkhipov <arhipov11@yandex.ru>
2022-02-25 19:50:44 +0300

    Amended PopupWithForm class

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,45 +1,24 @@
-import Popup from "./Popup.js";
+import PopupConfirm from "./PopupConfirm.js";
 
-export default class PopupWithForm extends Popup {
-  constructor({ popupSelector, handleFormSubmit }) {
-    super(popupSelector);
-    this._form = this._popup.querySelector(".form");
-    this._inputs = this._popup.querySelectorAll(".form__item");
-    this._submitButton = this._form.querySelector(".form__button")
-    this._handleFormSubmit = handleFormSubmit;
+export default class PopupWithForm extends PopupConfirm {
+  constructor({ popupSelector, handler }) {
+    super({ popupSelector, handler });
+    this._inputsList = this._popup.querySelectorAll(".form__item");
+    this._handleSubmit = handler;
   }
+  L;
 
   _getInputValues() {
     this._formValues = {};
-    this._inputs.forEach((input) => {
+    this._inputsList.forEach((input) => {
       this._formValues[input.name] = input.value;
     });
     return this._formValues;
   }
 
   setCurrentValues(values) {
-    this._inputs.forEach((input) => {
+    this._inputsList.forEach((input) => {
       input.value = values[input.name];
     });
-  }
-
-  setEventListeners() {
-    super.setEventListeners();
-    this._form.addEventListener("submit", (event) => {
-      this._handleFormSubmit(event, this._getInputValues());
-    });
-  }
-
-  close() {
-    super.close();
-    this._form.reset();
-  }
-
-  renderLoading(isLoading, text='Сохранить', textLoading = "Сохранение...") {
-    if (isLoading) {
-      this._submitButton.textContent = textLoading;
-    } else {
-      this._submitButton.textContent = text;
-    }
   }
 }
